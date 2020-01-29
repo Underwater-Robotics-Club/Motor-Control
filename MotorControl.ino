3b2cf82fb072fe532bd62e6ffacc9d75c2a86c48
mj08186 <54374205+mj08186@users.noreply.github.com>
2020-01-29 17:14:06 -0500

    Add files via upload

--- MotorControl.ino
+++ MotorControl.ino
@@ -1,28 +1,36 @@
-int motorPin = 11;
+int motorPin1 = 3;
+int motorPin2 = 3;
  
 void setup()
 {
+  //Setup Channel A
+  pinMode(20,OUTPUT); //Initiates Motor Channel A pin Direction
+  pinMode(22,OUTPUT); //Initiates Brake Channel A pin
+  pinMode(motorPin2, OUTPUT); //initiates Motor Channel B pin speed
   
   //Setup Channel B
-  pinMode(13, OUTPUT); //Initiates Motor Channel B pin
-  pinMode(8, OUTPUT); //Initiates Brake Channel B pin
-  pinMode(motorPin, OUTPUT); //initiates Motor Channel B pin speed
-  Serial.begin(9600);
-  while (! Serial);
-  Serial.println("Speed 0 to 255");
+  pinMode(21, OUTPUT); //Initiates Motor Channel B pin Direction
+  pinMode(23, OUTPUT); //Initiates Brake Channel B pin
+  pinMode(motorPin1, OUTPUT); //initiates Motor Channel B pin speed
 }
 
 void loop(){
   
-  //forward @ full speed
-  digitalWrite(13, HIGH); //Establishes forward direction of Channel B
-  digitalWrite(8, LOW);   //Disengage the Brake for Channel B
-  if (Serial.available())
+    if (Serial.available())
   {
     int speed = Serial.parseInt();
     if (speed >= 0 && speed <= 255)
+    
     {
-      analogWrite(motorPin, speed);
+      digitalWrite(20, HIGH); //Establishes forward direction of Channel B
+      digitalWrite(22, LOW);   //Disengage the Brake for Channel B
+      analogWrite(motorPin1, speed);
+
+      digitalWrite(21, HIGH); //Establishes forward direction of Channel B
+      digitalWrite(23, LOW);   //Disengage the Brake for Channel B
+      analogWrite(motorPin2, speed);
+
+      delay(1000);
     }
   }
   
